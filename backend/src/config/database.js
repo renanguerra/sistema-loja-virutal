6c20299d1a438a870e9fae0078d8ed2b8dda7cff
renanguerra <rebarbosa07@hotmail.com>
2020-10-03 19:14:04 -0300

    elephantsql

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '123',
-    database: 'poo',
+    url: 'postgres://smychxzj:8R3z8PBR-M5Z0IeM0TOmabEC_DthUZw2@motty.db.elephantsql.com:5432/smychxzj',
+    username: 'smychxzj',
+    password: '8R3z8PBR-M5Z0IeM0TOmabEC_DthUZw2',
+    database: 'projetos-medium',
     define: {
         timestamps: true, //usa o createAt e updateAt
         underscored: true, //usa o snake case tipo_isso naoIsso
