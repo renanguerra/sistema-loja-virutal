86262757c1dfefe54c579ee3c670a55a0e9c98ca
Renan Guerra <rebarbosa07@hotmail.com>
2020-10-07 08:08:15 -0300

    database configs

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    url: 'postgres://smychxzj:8R3z8PBR-M5Z0IeM0TOmabEC_DthUZw2@motty.db.elephantsql.com:5432/smychxzj',
-    username: 'smychxzj',
-    password: '8R3z8PBR-M5Z0IeM0TOmabEC_DthUZw2',
-    database: 'projetos-medium',
+    host: 'localhost',
+    username: 'user',
+    password: '',
+    database: 'poo',
     define: {
         timestamps: true, //usa o createAt e updateAt
         underscored: true, //usa o snake case tipo_isso naoIsso
